3c3910a658495950680c27518f2e86b7222e4647
Dan Fornika <dfornika@gmail.com>
2023-04-18 17:26:08 -0700

    Remove unused config

--- src/main/java/ca/bccdcphl/sequencingruns/config/api/ContentNegotiationConfig.java
+++
@@ -1,9 +0,0 @@
-package ca.bccdcphl.sequencingruns.config.api;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
-
-@Configuration
-public class ContentNegotiationConfig  {
-
-}
