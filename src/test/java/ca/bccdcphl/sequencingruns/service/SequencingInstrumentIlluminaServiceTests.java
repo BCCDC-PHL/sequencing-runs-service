b1b22ef4fce4910dc47f6d91d8b61702ae81a73f
Dan Fornika <dfornika@gmail.com>
2023-04-24 10:34:24 -0700

    Start building service tests

--- src/test/java/ca/bccdcphl/sequencingruns/service/SequencingInstrumentIlluminaServiceTests.java
+++ src/test/java/ca/bccdcphl/sequencingruns/service/SequencingInstrumentIlluminaServiceTests.java
@@ -1,24 +1,54 @@
 package ca.bccdcphl.sequencingruns.service;
 
+import ca.bccdcphl.sequencingruns.model.aggregates.SequencingInstrumentIllumina;
 import ca.bccdcphl.sequencingruns.repositories.SequencingInstrumentIlluminaRepository;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
 
-@ContextConfiguration(classes={
-        SequencingInstrumentIlluminaRepository.class
-})
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 public class SequencingInstrumentIlluminaServiceTests {
-
-    @InjectMocks
-    private SequencingInstrumentIlluminaService service;
 
     @Mock
     private SequencingInstrumentIlluminaRepository repo;
 
+    @InjectMocks
+    private SequencingInstrumentIlluminaService service;
+
     @Test
-    void contextLoads() {
+    void testGetInstrumentById() {
+        String TEST_INSTRUMENT_ID = "M00123";
+        SequencingInstrumentIllumina testInstrument = new SequencingInstrumentIllumina();
+        testInstrument.setInstrumentId(TEST_INSTRUMENT_ID);
+
+        when(repo.findByInstrumentId(TEST_INSTRUMENT_ID)).thenReturn(Optional.of(testInstrument));
+
+        Optional<SequencingInstrumentIllumina> result = service.getInstrumentById(TEST_INSTRUMENT_ID);
+        if (result.isPresent()) {
+            assertEquals(testInstrument, result.get());
+        }
+    }
+
+    @Test
+    void testGetInstrumentByNonexistentId() {
+        String NONEXISTENT_INSTRUMENT_ID = "FOO";
+
+        when(repo.findByInstrumentId(NONEXISTENT_INSTRUMENT_ID)).thenReturn(Optional.empty());
+
+        Optional<SequencingInstrumentIllumina> result = service.getInstrumentById(NONEXISTENT_INSTRUMENT_ID);
+        assert(result.isEmpty());
     }
 }
--- src/test/java/ca/bccdcphl/sequencingruns/service/SequencingInstrumentNanoporeServiceTests.java
+++ src/test/java/ca/bccdcphl/sequencingruns/service/SequencingInstrumentNanoporeServiceTests.java
@@ -1,21 +1,50 @@
 package ca.bccdcphl.sequencingruns.service;
 
+import ca.bccdcphl.sequencingruns.model.aggregates.SequencingInstrumentIllumina;
+import ca.bccdcphl.sequencingruns.model.aggregates.SequencingInstrumentNanopore;
+import ca.bccdcphl.sequencingruns.repositories.SequencingInstrumentIlluminaRepository;
 import ca.bccdcphl.sequencingruns.repositories.SequencingInstrumentNanoporeRepository;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ContextConfiguration;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Optional;
 
-@ContextConfiguration(classes={
-        SequencingInstrumentNanoporeRepository.class
-})
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 public class SequencingInstrumentNanoporeServiceTests {
 
     @Mock
     private SequencingInstrumentNanoporeRepository repo;
 
+    @InjectMocks
+    private SequencingInstrumentNanoporeService service;
+
     @Test
-    void contextLoads() {
+    void testGetInstrumentById() {
+        String TEST_INSTRUMENT_ID = "GXB00123";
+        SequencingInstrumentNanopore testInstrument = new SequencingInstrumentNanopore();
+        testInstrument.setInstrumentId(TEST_INSTRUMENT_ID);
+
+        when(repo.findByInstrumentId(TEST_INSTRUMENT_ID)).thenReturn(Optional.of(testInstrument));
+
+        Optional<SequencingInstrumentNanopore> result = service.getInstrumentById(TEST_INSTRUMENT_ID);
+        if (result.isPresent()) {
+            assertEquals(testInstrument, result.get());
+        }
+    }
+
+    @Test
+    void testGetInstrumentByNonexistentId() {
+        String NONEXISTENT_INSTRUMENT_ID = "FOO";
+
+        when(repo.findByInstrumentId(NONEXISTENT_INSTRUMENT_ID)).thenReturn(Optional.empty());
+
+        Optional<SequencingInstrumentNanopore> result = service.getInstrumentById(NONEXISTENT_INSTRUMENT_ID);
+        assert(result.isEmpty());
     }
 }
